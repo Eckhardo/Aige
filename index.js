0650daf96621d5f7d2ec70a5c47a5e88147265b0
Eckhardo <eckhard.kirschning@freenet.de>
2018-07-26 13:44:41 +0200

    Delete access keys

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
  * app.js with basic express configuration
  * 
- AWS_ACCESS_KEY='AKIAI3TXRY2BTPL5NP6A'  AWS_SECRET_KEY='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' S3_BUCKET='aige-file-upload' node index.js
+ 
  * Copyright (c) Eckhard Kirschning .
  */
 
@@ -28,10 +28,6 @@ var
        
 
 // ------------- END MODULE SCOPE VARIABLES ---------------
-var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY || 'AKIAI3TXRY2BTPL5NP6A' ;
-var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY || '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';
-var S3_BUCKET = process.env.S3_BUCKET || 'aige-file-upload';
-console.log(AWS_ACCESS_KEY + ", " + AWS_SECRET_KEY + "," + S3_BUCKET);
 // ------------- BEGIN SERVER CONFIGURATION ---------------
 
 
